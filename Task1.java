5911dff9f8c6cecbe50e6b7d79af4835a56631b5
rojina1 <rojinagurung58@gmail.com>
2025-12-26 13:13:18 +0545

    My Task1

--- Task1.java
+++ Task1.java
@@ -3,3 +3,11 @@ package Seven;
 public class Task1 {
 
 }
+class myName{
+	public void name() {
+		System.out.println("Rojina Gurung");
+	}
+	public void studentName() {
+		System.out.println("Rojina");
+	}
+}
